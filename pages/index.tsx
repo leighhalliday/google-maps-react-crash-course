615885a41d3ce90bfbfee0a1cf0ad8ed890529d2
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2023-07-31 22:18:43 +0300

    fix: performance issue (check description)
    esm.js:930 Performance warning! LoadScript has been reloaded unintentionally! You should not pass `libraries` prop as new array. Please keep an array of libraries as static class property for Components and PureComponents, or just a const variable outside of component, or somewhere in config files or ENV variables

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
-import { useLoadScript } from "@react-google-maps/api";
+import { useLoadScript, Libraries } from "@react-google-maps/api";
 import Map from "../components/map";
+import { useState } from "react";
 
 export default function Home() {
+  const [ libraries ] = useState(['places']) as Libraries[];
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-    libraries: ["places"],
+    libraries,
   });
 
   if (!isLoaded) return <div>Loading...</div>;
